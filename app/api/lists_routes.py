4f472b4dc3e572d8549a773e94fcb1313bbbfa70
Karandeep <jinnie1996@hotmail.com>
2022-02-18 17:27:20 -0800

    create list complete

--- app/api/lists_routes.py
+++ app/api/lists_routes.py
@@ -2,8 +2,19 @@ from flask import Blueprint, request
 from app.models import db, List
 from flask_login import current_user, login_required
 # from app.s3_helpers import (upload_file_to_s3, allowed_file, get_unique_filename)
+from app.forms.add_list_form import AddListForm
 
 lists_routes = Blueprint("lists", __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 @lists_routes.route('/user/<int:id>')
 @login_required
@@ -22,8 +33,22 @@ def getSingleList(id):
 
 @lists_routes.route('/', methods=["POST"])
 @login_required
-def postList(postId):
-    return
+def postList():
+    print("IN API!!!!!!", request.get_json()["title"])
+    data = request.get_json()
+    form = AddListForm()
+    print(form.data, "FORM DATA@!@#!#!@")
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        print("FORM@@@@@", form.data)
+        list = List(user_id=current_user.id, title=form.data["title"])
+        db.session.add(list)
+        db.session.commit()
+        return list.to_dict()
+    else:
+        print(form.errors)
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 @lists_routes.route('/<int:id>', methods=["PUT"])
 @login_required
