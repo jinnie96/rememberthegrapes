18a20e4ce1d20275d6e58519b421ece9396611d3
Karandeep <jinnie1996@hotmail.com>
2022-02-21 15:56:30 -0800

    proportions of page correct

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import './SplashPage.css'
 
 const SplashPage = () => {
     return (
@@ -9,16 +10,16 @@ const SplashPage = () => {
                 <div className="navBarLogo">
                     Remember the Grapes
                 </div>
-                <div className="LogSignUp">
+                <div className="logSignUp">
                     <p>Log In</p>
                     <p>Sign Up</p>
                 </div>
             </div>
             <div className="splashMiddle">
-
+                <h2>Middle</h2>
             </div>
             <div className="splashLower">
-
+            <h2>Lower</h2>
             </div>
         </div>
     )
